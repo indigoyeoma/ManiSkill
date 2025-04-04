23b7baadcc4dec60f4e04d899ef8cf6ac8bfef2a
Swahaha <swarajhmehta1@gmail.com>
2025-04-04 17:30:40 -0400

    fixed naming convention

--- hyperppo/hyper-ppo.py
+++ hyperppo/hyper-ppo.py
@@ -22,7 +22,7 @@ from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.vector.wrappers.gymnasium import ManiSkillVectorEnv
 
 # HyperPPO-specific import: Graph HyperNetwork-based actor
-from swa_model.core import hyperActor  # <-- reference to your HyperPPO code
+from model.core import hyperActor  # <-- reference to your HyperPPO code
 
 @dataclass
 class Args:
